3149259f711603be6c9b58f99057690c362a1135
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-05-09 21:02:31 +0700

    feat(global): Make dynamic repo & service injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Arr;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,10 +27,10 @@ class AppServiceProvider extends ServiceProvider
     {
         date_default_timezone_set('Asia/Kuala_Lumpur');
 
-        // DB::listen(function ($query) {
-        //     error_log($query->sql);
+        DB::listen(function ($query) {
+            error_log($query->sql);
             
-        // });
+        });
 
         $models = [
             'Activity',
@@ -36,13 +38,31 @@ class AppServiceProvider extends ServiceProvider
         ];
 
         //binding repository
-        foreach ($models as $model) {
-            $this->app->singleton("App\Repositories\Contracts\\{$model}RepositoryContract", "App\Repositories\Implementations\\{$model}RepositoryImplementation");
+        foreach ($models as $key => $value) {
+            if(is_array($value)) {
+                if(Arr::exists($value, "repository")) {
+                    $this->app->singleton("App\Repositories\Contracts\\{$key}RepositoryContract", $value["repository"]);
+                } else {
+                    $this->app->singleton("App\Repositories\Contracts\\{$key}RepositoryContract", "App\Repositories\Implementations\\{$key}RepositoryImplementation");
+                }
+            } else {
+                $this->app->singleton("App\Repositories\Contracts\\{$value}RepositoryContract", "App\Repositories\Implementations\\{$value}RepositoryImplementation");
+            }
+            
         }
 
         //binding services
-        foreach ($models as $model) {
-            $this->app->singleton("App\Services\Contracts\\{$model}ServiceContract", "App\Services\Implementations\\{$model}ServiceImplementation");
+        foreach ($models as $key => $value) {
+            if(is_array($value)) {
+                if(Arr::exists($value, "service")) {
+                    $this->app->singleton("App\Services\Contracts\\{$key}ServiceContract", $value["service"]);
+                } else {
+                    $this->app->singleton("App\Services\Contracts\\{$key}ServiceContract", "App\Services\Implementations\\{$key}ServiceImplementation");
+                }
+            } else {
+                $this->app->singleton("App\Services\Contracts\\{$value}ServiceContract", "App\Services\Implementations\\{$value}ServiceImplementation");
+            }
+            
         }
     }
 }
