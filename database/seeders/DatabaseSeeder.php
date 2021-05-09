f9c07659dbb4334d86b0fc7c617f46f83fd3cf8d
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-05-09 13:31:26 +0700

    feat: add runner database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(ActivitySeeder::class);
+        $this->call(HistorySeeder::class);
     }
 }
