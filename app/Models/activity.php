9bfeed8a7c02f289db32dc4219eb95396f6bb1a1
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-06-26 13:59:17 +0700

    fix(global): fix uppercase on model class

--- app/Models/activity.php
+++ app/Models/activity.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class activity extends Model
+class Activity extends Model
 {
     use HasFactory;
     use SoftDeletes;
--- app/Models/history.php
+++ app/Models/history.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class history extends Model
+class History extends Model
 {
     use HasFactory;
     use SoftDeletes;
